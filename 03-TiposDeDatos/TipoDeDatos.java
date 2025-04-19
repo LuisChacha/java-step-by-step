71730c3457ceb6c99f6d1c33d835f91be72e32a1
Luis Chacha <luischacha82@gmail.com>
2025-04-19 18:15:45 -0500

    Tipos de datos

---
+++ 03-TiposDeDatos/TipoDeDatos.java
@@ -0,0 +1,9 @@
+
+public class TipoDeDatos {
+
+    public static void main(String[] args) {
+        String nombre = "Luis";
+        System.out.printf("Nombre(string): %s%n", nombre);
+
+    }
+}
---
+++ 03-TiposDeDatos/TipoInt.java
@@ -0,0 +1,8 @@
+
+public class TipoInt {
+
+    public static void main(String[] args) {
+        int edad = 1;
+        System.out.printf("Edad es: %d%n", edad);
+    }
+}
