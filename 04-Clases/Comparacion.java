7b58fb9454152ac21fdcd7aed60c922dec9ea30a
Luis Chacha <luischacha82@gmail.com>
2025-04-20 20:22:52 -0500

    Comparaciones

--- 04-Clases/Comparacion.java
+++ 04-Clases/Comparacion.java
@@ -1,12 +1,12 @@
 public class Comparacion {
     public static void main(String[] args) {
-        double sueldo1=100000;
-        double sueldo2=100000;
-        if (sueldo1>sueldo2)
-            System.out.println("Sueldo1 > Sueldo2");
-        else if (sueldo1 < sueldo2)
-            System.out.println("Sueldo1 < Sueldo2");
-        else
-        System.out.println("sueldo1 = sueldo2");
+        double sueldo1=10000.51558;
+        double sueldo2=450055.052558;
+        System.out.println("sueldo1 > sueldo2: " + (sueldo1 > sueldo2));
+        System.out.println("sueldo1 < sueldo2: " + (sueldo1 < sueldo2));
+        System.out.println("sueldo1 >= sueldo2: " + (sueldo1 >= sueldo2));
+        System.out.println("sueldo1 <= sueldo2: " + (sueldo1 <= sueldo2));
+        System.out.println("sueldo1 != sueldo2: " + (sueldo1 != sueldo2));
+        System.out.println("sueldo1 == sueldo2: " + (sueldo1 == sueldo2));
     }
 }
