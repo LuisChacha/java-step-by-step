17740c323067d19614c6d38d147683721def7fc1
Luis Chacha <luischacha82@gmail.com>
2025-04-21 11:11:09 -0500

    Estructuras de control

---
+++ 05-EstructurasDeControl/SentenciaIf.java
@@ -0,0 +1,11 @@
+public class SentenciaIf {
+    public static void main(String[] args) {
+        int edad=18;
+        boolean puedeEntrar=edad>=18;
+        if (puedeEntrar)
+            System.out.println("Puede ingresar al lugar");
+        else
+            System.out.println("Solo se permiten mayores de edad");
+
+    }
+}
