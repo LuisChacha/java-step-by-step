47c6c11cf15884d47b039107ebc2aed8a3f59911
Luis Chacha <luischacha82@gmail.com>
2025-04-21 16:59:00 -0500

    Setencia For

--- 05-EstructurasDeControl/SentenciaFor.java
+++ 05-EstructurasDeControl/SentenciaFor.java
@@ -2,14 +2,14 @@ public class SentenciaFor {
     public static void main(String[] args) {
         System.out.println("Sentencia: For");
         int tabla = 2;
-        for (int j = 1; j <= 5; j++) {
+        tabla: for (int j = 1; j <= 5; j++) {
             System.out.printf("Tabla del: %d%n", j);
 
-            for (int i = 0; i <= 10; i++) {
-                //if (i % 2 == 0) {
+            digito: for (int i = 0; i <= 10; i++) {
+                if (i % 2 == 0) {
                     //continue;
-                    //break;
-                //}
+                    break tabla;
+                }
                 System.out.printf("%d * %d = %d%n", j, i, (j * i));
             }
             System.out.println(" ");
