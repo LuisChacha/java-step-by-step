8f56c0c0d8974c727a8cd5599374ba6fcbe6d7f6
Luis Chacha <luischacha82@gmail.com>
2025-04-20 20:15:13 -0500

    1

---
+++ 04-Clases/Comparacion.java
@@ -0,0 +1,12 @@
+public class Comparacion {
+    public static void main(String[] args) {
+        double sueldo1=100000;
+        double sueldo2=100000;
+        if (sueldo1>sueldo2)
+            System.out.println("Sueldo1 > Sueldo2");
+        else if (sueldo1 < sueldo2)
+            System.out.println("Sueldo1 < Sueldo2");
+        else
+        System.out.println("sueldo1 = sueldo2");
+    }
+}
