b7b0e385c8ca8ff6be525dc0b4b078b28e761087
Luis Chacha <luischacha82@gmail.com>
2025-07-27 12:52:50 -0500

    Operadores Logicos

---
+++ 03-TiposDeDatos/Logicos.java
@@ -0,0 +1,10 @@
+public class Logicos {
+    public static void main(String[] args) {
+        boolean a=true, b=false;
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        System.out.println("a && b: " + (a && b));
+        System.out.println("a || b: " + (a || b) );
+        System.out.println("!a: " + !a);
+    }
+}
